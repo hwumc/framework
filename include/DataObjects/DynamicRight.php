61ed006df11d3c5465e27a4abb3271c7dd2a5176
Simon Walker <github@stwalkerster.co.uk>
2013-07-25 13:29:44 +0100

    dynamic right data object

---
+++ include/DataObjects/DynamicRight.php
@@ -0,0 +1,59 @@
+<?php
+// check for invalid entry point
+if(!defined("HMS")) die("Invalid entry point");
+
+class DynamicRight extends DataObject
+{
+    
+    protected $right;
+    
+    public function getRight()
+    {
+        return $this->right;   
+    }
+    
+    public function setRight($right)
+    {
+        if(!$this->isNew)
+        {
+            throw new YouShouldntBeDoingThatException();
+        }
+        
+        $this->right = $right;
+    }
+    
+    public function delete() 
+    {
+        global $gDatabase;
+		$statement = $gDatabase->prepare("DELETE FROM pageright WHERE id = :id LIMIT 1;");
+		$statement->bindParam(":id", $this->id);
+		$statement->execute();
+
+		$this->id=0;
+		$this->isNew = true;
+    }
+    
+    public function save()
+    {
+        global $gDatabase;
+
+		if($this->isNew)
+		{ // insert
+			$statement = $gDatabase->prepare("INSERT INTO pageright VALUES (null, :right);");
+			$statement->bindParam(":right", $this->right);
+			if($statement->execute())
+			{
+				$this->isNew = false;
+				$this->id = $gDatabase->lastInsertId();
+			}
+			else
+			{
+				throw new SaveFailedException();
+			}
+		}
+		else
+		{ // update
+			throw new YouShouldntBeDoingThatException();
+		}
+    }
+}
