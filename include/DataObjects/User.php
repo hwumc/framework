0cdad2073e61cf70d65256112200f216423689d2
Simon Walker <github@stwalkerster.co.uk>
2013-10-02 00:08:24 +0100

    [HWUMC-5] email trip signup sheet

--- include/DataObjects/User.php
+++ include/DataObjects/User.php
@@ -47,6 +47,27 @@ class User extends DataObject
 		return $resultObject;
 	}
 
+    public static function getWithRight( $right ) 
+    {
+        global $gDatabase;
+		$statement = $gDatabase->prepare("SELECT DISTINCT u.* FROM `group` g INNER JOIN `rightgroup` rg ON rg.`group` = g.`id` INNER JOIN `usergroup` ug ON g.`id` = ug.`group` INNER JOIN `user` u ON u.`id` = ug.`user` WHERE `right` = :rght;");
+		$statement->bindParam(":rght", $right);
+
+		$statement->execute();
+
+		$resultObject = $statement->fetchAll( PDO::FETCH_CLASS, "User" );
+
+        $data = array();
+		foreach ($resultObject as $v)
+        {
+            $v->isNew = false;
+            $data[$v->getId() . ""] = $v;
+        }
+        
+		return $data;
+
+    }
+    
 	/**
 	 * Check the stored password against the provided password
 	 * @returns true if the password is correct
